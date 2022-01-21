eca4d9cf0fa8347aea3889c0a86305cc93109480
Matthew Laufer <matthew.j.laufer@gmail.com>
2022-01-20 22:31:40 -0800

    lualine: add terminal mode

--- lua/lualine/themes/undercity.lua
+++ lua/lualine/themes/undercity.lua
@@ -7,9 +7,10 @@ return {
         c = {bg = t.bg2, fg = t.fg2},
     },
     insert = {a = {bg = t.yellow, fg = t.bg1, gui = 'bold'}, b = {bg = t.bg1, fg = t.yellow}},
-    command = {a = {bg = t.green, fg = t.bg1, gui = 'bold'}, b = {bg = t.bg1, fg = t.green}},
     visual = {a = {bg = t.blue, fg = t.bg1, gui = 'bold'}, b = {bg = t.bg1, fg = t.blue}},
     replace = {a = {bg = t.magenta, fg = t.bg1, gui = 'bold'}, b = {bg = t.bg1, fg = t.magenta}},
+    command = {a = {bg = t.cyan, fg = t.bg1, gui = 'bold'}, b = {bg = t.bg1, fg = t.cyan}},
+    terminal = {a = {bg = t.green, fg = t.bg1, gui = 'bold'}, b = {bg = t.bg1, fg = t.green}},
     inactive = {
         a = {bg = t.bg2, fg = t.red, gui = 'bold'},
         b = {bg = t.bg1, fg = t.red},
